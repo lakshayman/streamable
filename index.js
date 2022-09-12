20ed89f8ce1c8ec450833d6829eb3404d0cef387
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2022-09-12 21:56:54 +0530

    Port added

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.get("/video", function (req, res) {
     videoStream.pipe(res);
 });
 
-app.listen(8000, function () {
+app.listen(process.env.PORT || 8000, function () {
     console.log("Listening on port 8000!");
 });
